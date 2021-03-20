22bc3b747cb9c912cde3984120451bd460c9864c
Sebastian Skałacki <skalee@gmail.com>
2021-03-20 18:24:24 +0100

    Scan HTML entities

--- lib/html2asciimath/converter.rb
+++ lib/html2asciimath/converter.rb
@@ -15,7 +15,7 @@ module HTML2AsciiMath
 
     def scan_input
       repeat_until_error_or_eos do
-        # TODO
+        scan_entity
         scan_error
       end
     end
@@ -30,8 +30,53 @@ module HTML2AsciiMath
       throw :error
     end
 
+    def scan_entity
+      str = scan(/&\w+;/) or return
+      ent_name = str[1..-2]
+
+      if allowed_entity?(ent_name)
+        symbol = ENTITY_TRANSLATIONS[ent_name] || ent_name
+        # TODO accept symbol
+        true
+      else
+        unscan
+        false
+      end
+    end
+
+    def allowed_entity?(ent_name)
+      ALLOWED_ENTITIES.include?(ent_name) ? ent_name : nil
+    end
+
     def to_asciimath
       return "Converted expression" # TODO
     end
+
+    GREEK_ALPHABET = %w[
+      alpha beta gamma delta epsilon zeta eta theta iota kappa lambda mu nu xi
+      omicron pi rho sigma tau upsilon phi chi psi omega
+    ].freeze
+
+    ENTITY_TRANSLATIONS = {
+      "ne" => "!=",
+      "lt" => "<",
+      "le" => "<=",
+      "gt" => ">",
+      "ge" => ">=",
+      "sdot" => "*",
+      "times" => "xx",
+      "divide" => "-:",
+      "infin" => "oo",
+      "forall" => "VV",
+      "exist" => "EE",
+    }.freeze
+
+    ALLOWED_ENTITIES = [
+      *GREEK_ALPHABET,
+      *GREEK_ALPHABET.map { |l| l.capitalize },
+      *GREEK_ALPHABET.map { |l| "var#{l}" }, # varpi, vartheta, etc.
+      *ENTITY_TRANSLATIONS.keys,
+      "not",
+    ].to_set.freeze
   end
 end
