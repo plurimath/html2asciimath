306639ffd98cbcc6225930d242dc70f4d289023b
Sebastian Skałacki <skalee@gmail.com>
2021-04-01 12:03:47 +0200

    Add some tests

--- spec/html2mathml_spec.rb
+++ spec/html2mathml_spec.rb
@@ -6,8 +6,4 @@ RSpec.describe HTML2MathML do
   it "has a version number" do
     expect(HTML2MathML::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
