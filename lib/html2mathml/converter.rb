acbf7f5730c497e61585aeb6143e8f0d75d315e9
Sebastian Skałacki <skalee@gmail.com>
2021-04-02 03:28:13 +0200

    wip

--- lib/html2mathml/converter.rb
+++ lib/html2mathml/converter.rb
@@ -7,46 +7,104 @@ require "strscan"
 
 module HTML2MathML
   class Converter < StringScanner
+    attr_reader :string, :html_scanner, :html_text_scanner
+
+    def initialize(str)
+      @string = str
+      @html_scanner = HTMLScanner.new(str)
+      @html_text_scanner = HTMLTextScanner.new
+    end
+
     def transform
-      scan_input
+      # scan_input
       to_math_ml
     end
 
     private
 
-    def scan_input
-      repeat_until_error_or_eos do
-        scan_html_element or scan_html_text or scan_error
-      end
+    def parse
+      html_scanner.parse
     end
 
-    def repeat_until_error_or_eos
-      catch(:error) do
-        yield until eos?
-      end
-    end
+    # def scan_input
+    #   repeat_until_error_or_eos do
+    #     scan_html_element or scan_html_text or scan_error
+    #   end
+    # end
 
-    def scan_html_element
-      str = scan(%r{</?\w+>}) or return
-      # TODO
-    end
 
-    def scan_html_text
-      str = scan(/[^<]+/) or return
-      str = decode_html(str).strip
-      # TODO
-    end
 
-    def scan_error
-      throw :error
-    end
+
+
+
+    # def parse_html_text(str)
+    #   str = decode_html(str)
+
+    #   while MATH_ITEM_RX =~ str
+
+    #   end
+
+    #   # TODO check if end
+
+    #   # str.scan(FORMULA_TOKEN_RX) do
+    #   # end
+    #   # text_scanner = StringScanner.new(decode_html(str))
+
+
+    #   interpret_token(token) while token = text_scanner.scan(FORMULA_TOKEN_RX)
+    # end
 
     def decode_html(str)
       CGI.unescapeHTML(str) # TODO CGI handles only some entities
     end
 
     def to_math_ml
+      parse
       "Converted expression" # TODO
+    end
+
+    MATH_ITEM_RX = %r{
+      \G (?:
+
+      )
+    }xi
+
+    class AbstractScanner < StringScanner
+      def repeat_until_error_or_eos
+        catch(:error) do
+          yield until eos?
+        end
+      end
+
+      def scan_error
+        throw :error
+      end
+    end
+
+    class HTMLScanner < AbstractScanner
+      def parse
+        repeat_until_error_or_eos do
+          scan_html_element or scan_html_text or scan_error
+        end
+
+        def scan_html_element
+          str = scan(%r{</?\w+>}) or return
+          # TODO
+        end
+
+        def scan_html_text
+          str = scan(/[^<]+/) or return
+          parent.parse_html_text(str)
+        end
+      end
+    end
+
+    class HTMLTextScanner < AbstractScanner
+      def parse
+        repeat_until_error_or_eos do
+          scan_ws or scan_number or scan_operator or scan_text
+        end
+      end
     end
   end
 end
