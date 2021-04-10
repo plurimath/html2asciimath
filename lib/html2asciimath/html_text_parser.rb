d0c3398752d78fc04127ccddd69ef56c4e5a8d77
Sebastian Skałacki <skalee@gmail.com>
2021-04-10 04:47:12 +0200

    Delete handling HTML entities from HTMLTextParser
    HTMLTextParser now receives decoded input.

--- lib/html2asciimath/html_text_parser.rb
+++ lib/html2asciimath/html_text_parser.rb
@@ -20,8 +20,7 @@ module HTML2AsciiMath
 
     def parse # rubocop:disable Metrics/CyclomaticComplexity
       repeat_until_error_or_eos do
-        skip_ws or scan_entity or scan_number or scan_symbol or
-          scan_text or scan_error
+        skip_ws or scan_number or scan_symbol or scan_text or scan_error
       end
     end
 
@@ -39,20 +38,6 @@ module HTML2AsciiMath
 
     def skip_ws
       skip(/\s+/)
-    end
-
-    def scan_entity
-      str = scan(/&\w+;/) or return
-      ent_name = str[1..-2]
-
-      if allowed_entity?(ent_name)
-        symbol = ENTITY_TRANSLATIONS[ent_name] || ent_name
-        push(symbol)
-        true
-      else
-        unscan
-        false
-      end
     end
 
     def scan_number
@@ -76,10 +61,6 @@ module HTML2AsciiMath
       true
     end
 
-    def allowed_entity?(ent_name)
-      ALLOWED_ENTITIES.include?(ent_name) ? ent_name : nil
-    end
-
     # Left side is a HTML math symbol recognized by scanner.  Right side is its
     # AsciiMath equivalent or nil when no translation is needed.
     # @todo Perhaps brackets should be handled separately.
@@ -99,32 +80,5 @@ module HTML2AsciiMath
     # A regular expression which matches every symbol defined in +SYMBOLS+ hash.
     SYMBOLS_RX =
       Regexp.new(SYMBOLS.keys.map { |k| Regexp.escape(k) }.join("|")).freeze
-
-    GREEK_ALPHABET = %w[
-      alpha beta gamma delta epsilon zeta eta theta iota kappa lambda mu nu xi
-      omicron pi rho sigma tau upsilon phi chi psi omega
-    ].freeze
-
-    ENTITY_TRANSLATIONS = {
-      "ne" => "!=",
-      "lt" => "<",
-      "le" => "<=",
-      "gt" => ">",
-      "ge" => ">=",
-      "sdot" => "*",
-      "times" => "xx",
-      "divide" => "-:",
-      "infin" => "oo",
-      "forall" => "VV",
-      "exist" => "EE",
-    }.freeze
-
-    ALLOWED_ENTITIES = [
-      *GREEK_ALPHABET,
-      *GREEK_ALPHABET.map { |l| l.capitalize },
-      *GREEK_ALPHABET.map { |l| "var#{l}" }, # varpi, vartheta, etc.
-      *ENTITY_TRANSLATIONS.keys,
-      "not",
-    ].to_set.freeze
   end
 end
