86ae12c4ccb3022d3b4bda9db58cb5eb023f926a
Sebastian Skałacki <skalee@gmail.com>
2021-04-14 20:56:38 +0200

    Write RDoc on Converter class

--- lib/html2asciimath/converter.rb
+++ lib/html2asciimath/converter.rb
@@ -3,6 +3,17 @@
 # (c) 2021 Ribose Inc.
 
 module HTML2AsciiMath
+  # This class is responsible for converting HTML math expressions to AsciiMath.
+  #
+  # It runs two small parsers: first HTMLparser deals with HTML syntax, and then
+  # HTMLTextParser processes textual content found between HTML elements.
+  # Thanks to this two-phase processing, HTMLTextParser receives input which is
+  # already decoded (i.e. without any HTML entities), which in turn allows to
+  # keep its grammar simple.
+  #
+  # @example
+  #   html_string = "<i>x</i>+<i>y</i>"
+  #   Converter.new(html_string).transform # => "x + y"
   class Converter
     attr_reader :ast, :ast_stack, :html_parser
     attr_accessor :variable_mode
